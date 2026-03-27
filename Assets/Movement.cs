12f9716371f4d5e8dad110c22ecc6db182825abd
ImISR43L <raeell26@gmail.com>
2026-03-27 16:06:25 -0300

    Alterações feitas

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        this.transform.position += new Vector3(horizontalInput*velocity,0,0);
+        this.transform.position += new Vector3(0,verticalInput*velocity,0);
+    }
+}
