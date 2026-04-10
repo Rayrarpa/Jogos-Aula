f5399678660956b5710c0a717d9bcf4970aebca4
ImISR43L <raeell26@gmail.com>
2026-04-10 14:58:29 -0300

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -18,5 +18,11 @@ public class Movement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         this.transform.position += new Vector3(horizontalInput*velocity,0,0);
         this.transform.position += new Vector3(0,verticalInput*velocity,0);
+        if(this.transform.position.x>10)
+        {
+            this.transform.position = new Vector3 (10, this.transform.position.y, this.transform.position.z);
+        } else if (this.transform.position.x<-10) {
+            this.transform.position = new Vector3 (-10, this.transform.position.y, this.transform.position.z);
+        }
     }
 }
