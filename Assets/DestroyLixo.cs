8c90d8c9c552f549b7ab4f6bb5143a40025a4844
mariana <rafadevvv@gmail.com>
2026-05-15 09:03:27 -0300

    zcscsa

--- Assets/DestroyLixo.cs
+++ Assets/DestroyLixo.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class DestroyLixo : MonoBehaviour
 {
+    public LixoSpawnerController lixoSpawnerController;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Lixo"))
         {
             Destroy(collision.gameObject);
+            lixoSpawnerController.AddToPoints(-1);
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
